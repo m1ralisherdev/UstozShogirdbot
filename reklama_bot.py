e556b091d009a7ac6e5df065a339e8680a940a7a
omon212 <omonnuloraimkulov@gmail.com>
2023-12-26 14:11:50 +0500

    excel edits

--- reklama_bot.py
+++ reklama_bot.py
@@ -6,32 +6,46 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode
 import openpyxl
+from openpyxl import Workbook
 
-API_TOKEN = '6514287083:AAF92CBSMQKpVXJM2gXwgSIfu7a5hceK5O0'
+# -----------------------------------------------------BOT
+API_TOKEN = '6705011112:AAEca6XiG4KYGRafdvJtkCug7kIyjLvaGHg'
 
 bot = Bot(token=API_TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot, storage=MemoryStorage())
 logging.basicConfig(level=logging.INFO)
 
-obunachilar = []
+# ----------------------------------------EXCEL
+
+wb = Workbook()
+ws = wb.active
+wb_read = openpyxl.load_workbook('database.xlsx')
+sheet = wb_read.active
+
+barcha_idlar_soni = sheet.max_row
+
+odamlar = []
 
 
+# ----------------------------------------DISPATCHER
 @dp.message_handler(commands='start')
 async def starter(message: Message):
-    if message.from_user.id not in obunachilar:
-        obunachilar.append(message.from_user.id)
-    await message.answer("Assalomaleykum Bot ga xush kelibsiz!")
+    for i in range(1, barcha_idlar_soni + 1):
+        id_telegram = sheet.cell(row=i, column=1).value
+        if id_telegram:
+            odamlar.append(int(id_telegram))
 
+    if message.from_user.id not in odamlar:
+        odamlar.append(message.from_user.id)
+        await message.answer("Assalomaleykum Bot ga xush kelibsiz!\n siz uchun bonuslarimiz mavjud")
 
-@dp.message_handler(text='reklama_admin')
-async def rekalma(message: Message):
-    if message.from_user.id == 5172746353:
-        for i in obunachilar:
-            with open("moshina.jpg", 'rb') as photo:
-                await bot.send_photo(i, photo,
-                                     caption="Yangi kuchuklar sotiladi arzon narxlarda Dastavchik Ibrohim")
     else:
-        await message.answer("Siz admin emas siz")
+        await message.answer(
+            "Assalomaleykum Siz botdan oldin ro`yxatdan o`tib bo`lgansiz siz uchun bonuslar mavjud emas!")
+
+    for i in range(1, len(odamlar) + 1):
+        ws[f'A{i}'] = odamlar[i - 1]
+    wb.save('database.xlsx')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
