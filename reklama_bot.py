55ffdaa099bf83a49a204041a6819788e3f91f9a
m1ralisherdev <153814334+m1ralisherdev@users.noreply.github.com>
2023-12-28 14:29:47 +0500

    Update reklama_bot.py

--- reklama_bot.py
+++ reklama_bot.py
@@ -1,22 +1,21 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import Message, ContentType
+from aiogram.types import Message
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode
 import openpyxl
 from openpyxl import Workbook
+import datetime
 
-# -----------------------------------------------------BOT
-API_TOKEN = '6705011112:AAEca6XiG4KYGRafdvJtkCug7kIyjLvaGHg'
-
+# Bot initialization
+API_TOKEN = '6580326658:AAG3oFYRU-FeqxoqHn0-_MluEWKaceT8eao'
 bot = Bot(token=API_TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot, storage=MemoryStorage())
 logging.basicConfig(level=logging.INFO)
 
-# ----------------------------------------EXCEL
-
+# Excel initialization
 wb = Workbook()
 ws = wb.active
 wb_read = openpyxl.load_workbook('database.xlsx')
@@ -25,48 +24,44 @@ sheet = wb_read.active
 barcha_idlar_soni = sheet.max_row
 
 odamlar = []
-
 vaqtlar = []
 
-import datetime
+# Load existing user data
+for i in range(1, barcha_idlar_soni + 1):
+    id_telegram = sheet.cell(row=i, column=1).value
+    if id_telegram:
+        odamlar.append(int(id_telegram))
+
+    vaqt_qoshilgan = sheet.cell(row=i, column=2).value
+    if vaqt_qoshilgan:
+        vaqtlar.append(vaqt_qoshilgan)
 
 
-
-
-# ----------------------------------------DISPATCHER
+# Dispatcher
 @dp.message_handler(commands='start')
 async def starter(message: Message):
     x = datetime.datetime.now()
     now = f"{x.strftime('%d')}/{x.strftime('%X')}"
-    for i in range(1, barcha_idlar_soni + 1):
-        id_telegram = sheet.cell(row=i, column=1).value
-        if id_telegram:
-            odamlar.append(int(id_telegram))
-    for i in range(1, barcha_idlar_soni + 1):
-        vaqt_qoshilgan = sheet.cell(row=i, column=2).value
-        if vaqt_qoshilgan:
-            vaqtlar.append(vaqt_qoshilgan)
-    if message.from_user.id not in odamlar:
-        print(True)
-
-        vaqtlar.append(now)
 
     if message.from_user.id not in odamlar:
         odamlar.append(message.from_user.id)
+        vaqtlar.append(now)
+
         for i in range(1, len(odamlar) + 1):
-            print(vaqtlar)
-            ws[f'B{i}'] = vaqtlar[i - 1]
-        for i in range(1, len(odamlar) + 1):
-            print(odamlar)
             ws[f'A{i}'] = odamlar[i - 1]
+            if vaqtlar[i - 1].startswith(x.strftime('%d')):
+                ws[f'A{i}'].font = openpyxl.styles.Font(color="00FF00")
+            else:
+                ws[f'A{i}'].font = openpyxl.styles.Font(color="FF0000")  #
+
+            ws[f'B{i}'] = vaqtlar[i - 1]
 
         wb.save('database.xlsx')
-        print(True, 1)
-        await message.answer("Assalomaleykum Bot ga xush kelibsiz!\n siz uchun bonuslarimiz mavjud")
+        await message.answer("Assalomaleykum Bot ga xush kelibsiz!\nSiz uchun bonuslarimiz mavjud")
 
     else:
         await message.answer(
-            "Assalomaleykum Siz botdan oldin ro`yxatdan o`tib bo`lgansiz siz uchun bonuslar mavjud emas!")
+            "Assalomaleykum! Siz botdan oldin ro`yxatdan o`tib bo`lgansiz. Siz uchun bonuslar mavjud emas!")
 
 
 if __name__ == '__main__':
